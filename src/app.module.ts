75412907999eceb09883d8ab0c56e19b1f9307fd
Kangoula <g-denis@ik.me>
2025-08-05 11:49:52 +0200

    create structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
+  imports: [RecipesModule],
   controllers: [AppController],
 })
 export class AppModule {}
