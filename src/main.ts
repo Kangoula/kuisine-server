68660ab29a4dddcdb692b012129fbcdc70ac4128
Kangoula <g-denis@ik.me>
2025-08-08 16:00:45 +0200

    prettier

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('query parser', 'extended');
 
-  app.useGlobalPipes(new ValidationPipe({transform: true, transformOptions: {enableImplicitConversion: true}}));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   app.useGlobalFilters(new EntityNotFoundErrorFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
