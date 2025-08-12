f435c01edb8256455ba62e858a2a0ef2ef949d1b
Kangoula <g-denis@ik.me>
2025-08-12 13:48:05 +0200

    enable compression

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { EntityNotFoundErrorFilter } from './common/filters/entity-not-found-error.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('query parser', 'extended');
+  app.use(compression());
 
   app.useGlobalPipes(
     new ValidationPipe({
