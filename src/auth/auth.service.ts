1c33399b10d5418fa4110f094cbe071d4a418fe0
Kangoula <g-denis@ik.me>
2025-08-19 16:40:38 +0200

    validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
+import { User } from '@/users/entities/user.entity';
+import { UsersService } from '@/users/users.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(public usersService: UsersService) {}
+
+  public async validateUser(
+    username: string,
+    password: string,
+  ): Promise<boolean> {
+    const user = await this.usersService.findOneBy({
+      username,
+      password: await this.usersService.hashPassword(password),
+    });
+
+    return Boolean(user);
+  }
+}
