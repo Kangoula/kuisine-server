af97d2d7a19b0a8784c5e344429aa37fc0525854
Kangoula <g-denis@ik.me>
2025-08-20 10:14:53 +0200

    use number constructor instead of ternary

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+  port: Number(process.env.DB_PORT ?? 5432),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
