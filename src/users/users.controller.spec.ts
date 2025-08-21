6be2ff74467152d3334503d2db1c6318bf06c0c7
Kangoula <g-denis@ik.me>
2025-08-21 12:18:44 +0200

    find one and update

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TestBed } from '@suites/unit';
 import { Mocked } from '@suites/doubles.jest';
+import { EntityNotFoundError } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -46,5 +47,53 @@ describe('UsersController', () => {
 
     expect(service.paginate).toHaveBeenCalledWith({ page: 1, perPage: 10 });
     expect(returnedUsers).toBe(users);
+  });
+
+  it('should return the requested user', async () => {
+    const userId = 1;
+    const user = {
+      id: userId,
+      username: 'Schwold Arnarzenegger',
+      password: 'MonsieurGelDansChauveSourisHomme',
+    };
+
+    service.findOneOrFail.mockResolvedValue(user);
+
+    const returnedUser = await controller.findOne(userId);
+
+    expect(service.findOneOrFail).toHaveBeenCalledWith(userId);
+    expect(returnedUser).toBe(user);
+  });
+
+  it('should throw an error when the requested user does not exists', () => {
+    const userId = 1;
+    const err = new Error('not found');
+
+    service.findOneOrFail.mockImplementation(() => {
+      return Promise.reject(err);
+    });
+
+    //eslint-disable-next-line @typescript-eslint/no-floating-promises
+    expect(controller.findOne(userId)).rejects.toBe(err);
+  });
+
+  it('should update user', async () => {
+    const user = {
+      id: 1,
+      username: 'Schwold Arnarzenegger',
+      password: 'MonsieurGelDansChauveSourisHomme',
+    };
+    const updateData = { username: 'Clorge Geooney' };
+
+    service.update.mockResolvedValue({
+      affected: 1,
+      raw: [],
+      generatedMaps: [],
+    });
+
+    const result = await controller.update(user.id, updateData);
+
+    expect(service.update).toHaveBeenCalledWith(user.id, updateData);
+    expect(result.affected).toBe(1);
   });
 });
