cce64457315f4475085fe5642cedc5c1e061bbeb
Kangoula <g-denis@ik.me>
2025-08-21 11:04:57 +0200

    start controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,50 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TestBed } from '@suites/unit';
+import { Mocked } from '@suites/doubles.jest';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: Mocked<UsersService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+    const { unit, unitRef } = await TestBed.solitary(UsersController).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    controller = unit;
+    service = unitRef.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create the given user', async () => {
+    const userData = {
+      username: 'Seaven Stegal',
+      password: 'MielEnHautePierre',
+    };
+
+    const expectedUser = {
+      id: 1,
+      username: 'Seaven Stegal',
+      password: 'MielEnHautePierre',
+    };
+
+    service.create.mockResolvedValue(expectedUser);
+
+    const createdUser = await controller.create(userData);
+
+    expect(service.create).toHaveBeenCalledWith(userData);
+    expect(createdUser).toBe(expectedUser);
+  });
+
+  it('should return a list of users', async () => {
+    const users = [
+      { id: 1, username: 'Seaven Stegal', password: 'MielEnHautePierre' },
+      { id: 2, username: 'Nuck Chorris', password: 'TalkerRexasWanger' },
+    ];
+
+    service.paginate.mockResolvedValue(users);
+
+    const returnedUsers = await controller.findAll({ page: 1, perPage: 10 });
+
+    expect(service.paginate).toHaveBeenCalledWith({ page: 1, perPage: 10 });
+    expect(returnedUsers).toBe(users);
   });
 });
