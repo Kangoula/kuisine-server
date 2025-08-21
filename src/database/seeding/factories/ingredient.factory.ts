d03befda9d090dfbdea9be34a82cfbe03dcddd37
Kangoula <g-denis@ik.me>
2025-08-21 17:09:33 +0200

    fix possible duplicates

--- src/database/seeding/factories/ingredient.factory.ts
+++ src/database/seeding/factories/ingredient.factory.ts
@@ -1,10 +1,25 @@
 import { Ingredient } from '@/ingredients/entities/ingredient.entity';
+import { Faker } from '@faker-js/faker';
 import { setSeederFactory } from 'typeorm-extension';
+
+const usedNames: string[] = [];
+
+// it could lead to an infinite recursion loop,
+// but we use the factory in dev only
+const _generateName = (faker: Faker): string => {
+  const name: string = faker.lorem.words({ min: 1, max: 3 });
+  if (usedNames.includes(name)) {
+    return _generateName(faker);
+  }
+
+  usedNames.push(name);
+  return name;
+};
 
 export const IngredientFactory = setSeederFactory(Ingredient, (faker) => {
   const ingredient = new Ingredient();
 
-  ingredient.name = faker.lorem.words({ min: 1, max: 3 });
+  ingredient.name = _generateName(faker);
 
   return ingredient;
 });
