d622ff2966c9e65ef4c7a4c77bbb062ed6a78a67
Kangoula <g-denis@ik.me>
2025-08-27 16:27:07 +0200

    move database config to a scoped config service

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,8 +1,22 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-import typeormConfig from './typeorm.config';
+import { SeederOptions } from 'typeorm-extension';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
-// nécessaire pour les migrations
-const dataSource = new DataSource(typeormConfig as DataSourceOptions);
+// datasource used for the migrations & seeders
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  namingStrategy: new SnakeNamingStrategy(),
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  seeds: ['dist/database/seeding/**/*.seeder{.ts,.js}'],
+  factories: ['dist/database/seeding/factories/**/*.factory{.ts,.js}'],
+} as DataSourceOptions & SeederOptions);
 
 export default dataSource;
 
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import typeormConfig from './typeorm.config';
+import { ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig)],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.user'),
+          password: configService.get<string>('database.password'),
+          database: configService.get<string>('database.name'),
+          synchronize: false,
+          namingStrategy: new SnakeNamingStrategy(),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
 })
 export class DatabaseModule {}
