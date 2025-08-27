880a9e8e88e2b4104d22d31720675a211a896e4a
Kangoula <g-denis@ik.me>
2025-08-27 14:40:16 +0200

    add tests for register

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@suites/unit';
 import { UsersService } from '@/users/users.service';
 import { Mocked } from '@suites/doubles.jest';
 import { bcryptHash } from '@/common/helpers/bcryptHash';
-import { BadRequestException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PostgresErrorCode } from '@/database/postgresErrorCodes.enum';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -52,5 +56,69 @@ describe('AuthService', () => {
     );
 
     expect(usersSerivce.findByUsernameWithPassword).toHaveBeenCalled();
+  });
+
+  it('should register a new user', async () => {
+    const expectedUserId = 1;
+    const username = 'Nuck Chorris';
+    const password = 'pw';
+
+    usersSerivce.create.mockResolvedValue({
+      id: expectedUserId,
+      username,
+    });
+
+    const result = await service.register({ username, password });
+
+    expect(result).toBeTruthy();
+    expect(result.username).toBe(username);
+    expect(result.id).toBe(expectedUserId);
+
+    expect(usersSerivce.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        username,
+        password,
+      }),
+    );
+  });
+
+  it('register should throw BadRequest if username already exists', async () => {
+    const username = 'Nuck Chorris';
+    const password = 'pw';
+
+    usersSerivce.create.mockRejectedValue({
+      code: PostgresErrorCode.UniqueViolation,
+    });
+
+    await expect(service.register({ username, password })).rejects.toThrow(
+      new BadRequestException('A User with this username already exists'),
+    );
+
+    expect(usersSerivce.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        username,
+        password,
+      }),
+    );
+  });
+
+  it('register should throw InternalServerError', async () => {
+    const username = 'Nuck Chorris';
+    const password = 'pw';
+
+    usersSerivce.create.mockRejectedValue(
+      'error that is not a postgres unique violation error',
+    );
+
+    await expect(service.register({ username, password })).rejects.toThrow(
+      new InternalServerErrorException(),
+    );
+
+    expect(usersSerivce.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        username,
+        password,
+      }),
+    );
   });
 });
