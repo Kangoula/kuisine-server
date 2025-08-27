069eb4f31b9f493db8e2acab4a61f928e19a0c0e
Kangoula <g-denis@ik.me>
2025-08-27 17:15:00 +0200

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,19 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as compression from 'compression';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Kuisine')
+    .setDescription('Public API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -16,6 +29,8 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.useGlobalFilters(new EntityNotFoundErrorFilter());
+
+  setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
