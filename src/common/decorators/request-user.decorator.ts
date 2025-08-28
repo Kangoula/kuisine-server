e1f7e959711e3d9068b9657ac5ca2dc6fd69c68a
Guillaume Denis <g-denis@ik.me>
2025-08-28 10:27:56 +0200

    Feature request user decorator (#36)

---
+++ src/common/decorators/request-user.decorator.ts
@@ -0,0 +1,15 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserWithoutCredentials } from '@/users/dto/user-without-credentials.dto';
+
+type ReqWithUser = { user: UserWithoutCredentials };
+
+export const requestUserFactory = (data: unknown, ctx: ExecutionContext) => {
+  const request: ReqWithUser = ctx.switchToHttp().getRequest();
+
+  return request.user;
+};
+
+/**
+ * Returns the authenticated user of the request
+ */
+export const RequestUser = createParamDecorator(requestUserFactory);
