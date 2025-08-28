6a2eff1a928e220cd2ca766d42fd01dcbe986ec2
Kangoula <g-denis@ik.me>
2025-08-28 10:26:09 +0200

    add decorator

---
+++ src/common/decorators/request-user.decorator.spec.ts
@@ -0,0 +1,26 @@
+import { ExecutionContext } from '@nestjs/common';
+import { UserWithoutCredentials } from '@/users/dto/user-without-credentials.dto';
+import { requestUserFactory } from './request-user.decorator';
+
+describe('Request User Param Decorators', () => {
+  const makeContext = (user: UserWithoutCredentials | undefined) => {
+    return {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          user,
+        }),
+      }),
+    } as unknown as ExecutionContext;
+  };
+
+  it('should return the request user', () => {
+    const expectedUser = { id: 2, username: 'Lucien Marcheciel' };
+    const result = requestUserFactory(null, makeContext(expectedUser));
+    expect(result).toEqual(expect.objectContaining(expectedUser));
+  });
+
+  it('should return undefined when no user is found', () => {
+    const result = requestUserFactory(null, makeContext(undefined));
+    expect(result).toBeUndefined();
+  });
+});
