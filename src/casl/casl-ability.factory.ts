dfe66a29dfac8fc09b8eac3ba424538fb0a883cc
Kangoula <g-denis@ik.me>
2025-08-29 09:51:44 +0200

    create abilities for user

--- src/casl/casl-ability.factory.ts
+++ src/casl/casl-ability.factory.ts
@@ -9,6 +9,7 @@ import { Action } from './action.enum';
 import { BaseEntity } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { UserWithoutCredentials } from '@/users/dto/user-without-credentials.dto';
+import { Permission } from '@/roles/entities/role.entity';
 
 type Subjects = InferSubjects<typeof BaseEntity> | 'all';
 
@@ -21,6 +22,52 @@ export class CaslAbilityFactory {
 
     if (user.role.name === 'Admin') {
       can(Action.Manage, 'all'); // read-write access to everything
+    } else {
+      const roleUserAbilities: Permission[] = [
+        {
+          subject: 'all',
+          action: 'read',
+        },
+        {
+          subject: 'Ingredient',
+          action: 'create',
+        },
+        {
+          subject: 'Ingredient',
+          action: ['update', 'delete'],
+          conditions: {
+            own: true,
+          },
+        },
+      ];
+
+      // we can update or delete our own profile
+      can(['update', 'delete'], 'User', { id: user.id });
+
+      roleUserAbilities.forEach((ability: Permission) => {
+        let conditions = ability.conditions;
+        if (conditions?.own === true) {
+          //eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { own, ...rest } = conditions;
+          conditions = Object.assign(rest, { userId: user.id });
+        }
+
+        if (ability.inverted) {
+          cannot(
+            ability.action,
+            ability.subject,
+            ability.fields,
+            ability.conditions,
+          );
+        } else {
+          can(
+            ability.action,
+            ability.subject,
+            ability.fields,
+            ability.conditions,
+          );
+        }
+      });
     }
 
     // TODO retrieve permissions from DB
