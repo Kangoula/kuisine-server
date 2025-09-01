fa7475c9be0b4d5fef47c359fcfab6c03c64b1ca
Kangoula <g-denis@ik.me>
2025-09-01 14:03:35 +0200

    add permissions check

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,33 +14,41 @@ import { EntityId } from '@/common/decorators/route-params.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { UserWithoutCredentials } from './dto/user-without-credentials.dto';
+import { Permission } from '@/casl/permission.decorator';
+import { Action } from '@/casl/action.enum';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Permission(User, Action.Read)
   @ApiOkResponse({ type: UserWithoutCredentials, isArray: true })
   findAll(@Query() paginationDto: PaginationDto) {
     return this.usersService.paginate(paginationDto);
   }
 
   @Get(':id')
+  @Permission(User, Action.Read)
   findOne(@EntityId() id: number) {
     return this.usersService.findOne(id);
   }
 
   @Post()
+  @Permission(User, Action.Create)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Patch(':id')
+  @Permission(User, Action.Update)
   update(@EntityId() id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @Permission(User, Action.Delete)
   async remove(@EntityId() id: number) {
     await this.usersService.findOne(id);
     return this.usersService.remove(id);
