af296079dc996f2286295f1b8275fdeb404492a0
Kangoula <g-denis@ik.me>
2025-09-02 15:51:43 +0200

    remove one nested level of describe

--- test/ingredients.e2e-spec.ts
+++ test/ingredients.e2e-spec.ts
@@ -17,73 +17,71 @@ describe('Ingredients', () => {
     await app.init();
   });
 
-  describe('ingredients', () => {
-    describe('unauthenticated', () => {
-      it('/POST should fail with Unauthorized response', async () => {
-        const response = await request(app.getHttpServer())
-          .post('/ingredients')
-          .send({ name: 'durian' });
+  describe('unauthenticated', () => {
+    it('/POST should fail with Unauthorized response', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/ingredients')
+        .send({ name: 'durian' });
 
-        expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
-      });
+      expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
+    });
+  });
+
+  describe('authenticated as admin', () => {
+    let bearerToken: string;
+
+    beforeEach(async () => {
+      bearerToken = await loginAs(app, 'admin');
     });
 
-    describe('authenticated', () => {
-      let bearerToken: string;
+    it('/POST with correct payload should return the created ingredient with Created response', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/ingredients')
+        .set('Authorization', bearerToken)
+        .send({ name: 'durian' });
 
-      beforeEach(async () => {
-        bearerToken = await loginAs(app, 'admin');
+      expect(response.status).toBe(HttpStatus.CREATED);
+      expect(response.body).toHaveProperty('id', expect.any(Number));
+      expect(response.body).toHaveProperty('name', 'durian');
+      expect(response.body).not.toHaveProperty('deletedAt');
+      expect(response.body).not.toHaveProperty('fullTextSeach');
+    });
+
+    it('/POST with incorrect payload should fail with Bad Request response', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/ingredients')
+        .set('Authorization', bearerToken)
+        .send({ name: '' });
+
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('/GET/search should return expected ingredient in fulltext search', async () => {
+      const ingredientService = app.get(IngredientsService);
+      const expectedName = 'tomate';
+
+      await ingredientService.create({
+        name: expectedName,
       });
 
-      it('/POST with correct payload should return the created ingredient with Created response', async () => {
-        const response = await request(app.getHttpServer())
-          .post('/ingredients')
-          .set('Authorization', bearerToken)
-          .send({ name: 'durian' });
+      const response = await request(app.getHttpServer())
+        .get('/ingredients/search')
+        .query({ term: 'tom' })
+        .set('Authorization', bearerToken);
 
-        expect(response.status).toBe(HttpStatus.CREATED);
-        expect(response.body).toHaveProperty('id', expect.any(Number));
-        expect(response.body).toHaveProperty('name', 'durian');
-        expect(response.body).not.toHaveProperty('deletedAt');
-        expect(response.body).not.toHaveProperty('fullTextSeach');
-      });
+      expect(response.status).toBe(HttpStatus.OK);
+      expect(response.body[0]).toBeDefined();
+      expect(response.body[0].name).toBe(expectedName);
+    });
 
-      it('/POST with incorrect payload should fail with Bad Request response', async () => {
-        const response = await request(app.getHttpServer())
-          .post('/ingredients')
-          .set('Authorization', bearerToken)
-          .send({ name: '' });
+    it('/GET/search should not return anything in fulltext search', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/ingredients/search')
+        .query({ term: 'xb2212' })
+        .set('Authorization', bearerToken);
 
-        expect(response.status).toBe(HttpStatus.BAD_REQUEST);
-      });
-
-      it('/GET/search should return expected ingredient in fulltext search', async () => {
-        const ingredientService = app.get(IngredientsService);
-        const expectedName = 'tomate';
-
-        await ingredientService.create({
-          name: expectedName,
-        });
-
-        const response = await request(app.getHttpServer())
-          .get('/ingredients/search')
-          .query({ term: 'tom' })
-          .set('Authorization', bearerToken);
-
-        expect(response.status).toBe(HttpStatus.OK);
-        expect(response.body[0]).toBeDefined();
-        expect(response.body[0].name).toBe(expectedName);
-      });
-
-      it('/GET/search should not return anything in fulltext search', async () => {
-        const response = await request(app.getHttpServer())
-          .get('/ingredients/search')
-          .query({ term: 'xb2212' })
-          .set('Authorization', bearerToken);
-
-        expect(response.status).toBe(HttpStatus.OK);
-        expect(response.body).toHaveLength(0);
-      });
+      expect(response.status).toBe(HttpStatus.OK);
+      expect(response.body).toHaveLength(0);
     });
   });
 
