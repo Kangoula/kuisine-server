47e07594ce795755cef542186d4c44c5db76ed8d
Kangoula <g-denis@ik.me>
2025-09-02 12:27:31 +0200

    fix seeder

--- src/database/seeding/user.seeder.ts
+++ src/database/seeding/user.seeder.ts
@@ -2,6 +2,8 @@ import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { User } from '@/users/entities/user.entity';
 import { bcryptHash } from '@/common/helpers/bcryptHash';
+import dataSource from '../data-source';
+import { Role } from '@/roles/entities/role.entity';
 
 export class UserSeeder implements Seeder {
   public async run(
@@ -18,16 +20,20 @@ export class UserSeeder implements Seeder {
 
     const password = await bcryptHash('xb2212');
 
+    const roleRepo = dataSource.manager.getRepository(Role);
+    const adminRole = await roleRepo.findOneByOrFail({ isAdmin: true });
+    const userRole = await roleRepo.findOneByOrFail({ name: 'User' });
+
     await userFactory.save({
       username: 'admin',
       password,
-      roleId: 1,
+      roleId: adminRole.id,
     });
 
     await userFactory.save({
       username: 'jabba',
       password,
-      roleId: 2,
+      roleId: userRole.id,
     });
 
     console.log('test user created');
