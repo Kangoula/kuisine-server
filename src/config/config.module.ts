e833d00358eeb26af2e67263d6f6786553fc6344
Kangoula <g-denis@ik.me>
2025-09-05 09:25:27 +0200

    export config module

---
+++ src/config/config.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import authConfig from './auth.config';
+import databaseConfig from './database.config';
+import * as Joi from '@hapi/joi';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      load: [authConfig, databaseConfig],
+      // ensure all env variables are present before starting
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        JWT_ACCESS_SECRET: Joi.string().required(),
+        JWT_ACCESS_EXPIRATION_TIME: Joi.string().required(),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        JWT_REFRESH_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
+  ],
+})
+export class ConfigModule {}
